6ab2fb40bed6dcef56d1c0e47fa4fa8809341dbf
Nicole Bahamón Martínez <n.bahamon@uniandes.edu.co>
2023-11-26 14:07:06 -0500

    Update 2_🔨_App.py

--- pages/2_🔨_App.py
+++ pages/2_🔨_App.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:285fc033d3629f522d6be13a67112e258afc5a1b7987efc6c2c4b3eb147f3b03
-size 2474
+oid sha256:245e19d2a5804c41b561067616637bf6bef7b2adcbe3f853ee9ba1f9d7cecf6a
+size 2470
